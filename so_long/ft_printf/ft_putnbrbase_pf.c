1a8703fd0fd282f21f9e2c73c5055d2b2c44d940
Isabel Frías Castillo <141316576+ifriasc@users.noreply.github.com>
2024-07-04 18:54:29 +0200

    ft_printf 's so_long upload

---
+++ so_long/ft_printf/ft_putnbrbase_pf.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbrbase_pf.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ifrias-c <ifrias-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 13:36:06 by ifrias-c          #+#    #+#             */
+/*   Updated: 2024/07/04 17:41:44 by ifrias-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putnbrbase_pf(unsigned long n, int base, int begin)
+{
+	int	length;
+
+	length = 0;
+	if (n < (unsigned long) base)
+	{
+		if (n <= 9)
+			return (ft_putchar_pf(n + '0'));
+		else
+		{
+			if (begin == 'x')
+				return (ft_putchar_pf((n % 10) + 'a'));
+			else
+				return (ft_putchar_pf((n % 10) + 'A'));
+		}
+	}
+	else
+	{
+		length += ft_putnbrbase_pf((unsigned long) n / base, base, begin);
+		length += ft_putnbrbase_pf((unsigned long) n % base, base, begin);
+	}
+	return (length);
+}
